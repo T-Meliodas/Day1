317824f8e2dffb98dc8d5b6aaf781cae2ef2595a
thanh <tthhaannhh07@gmail.com>
2024-12-04 12:20:23 +0700

    comment array

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 var myArr = ["java", "Python", "PHP"];
 var myArr2 = ["thanh", 123];
-var noi = myArr.concat(myArr2);
-console.log(noi.slice(0));
+var noi = myArr.concat(myArr2);// nối 2 array
+console.log(noi.slice(0));//cắt tất cả các phần tử trong array(từ vị trí 0)
